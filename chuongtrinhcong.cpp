1d1f6dd7a18d75b13e825904f796fb0bd8a97847
nnhaiNam <hainam000090@gmail.com>
2023-09-25 11:00:01 +0700

    Them file thantuong.txt va chuongtrinhcong.cpp

---
+++ chuongtrinhcong.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Nhap hai so a,b: ";
+    cin>>a>>b;
+    cout<<"Tong hai so "<<a<<" va "<<b<<" la: "<<a+b;
+    return 0;
+
+
+
+
+}
